2f13c05ed99046e851dcc7855f39f97fc1cb5190
olesiamelnychyn <43441208+olesiamelnychyn@users.noreply.github.com>
2018-11-14 15:09:45 +0100

    Add files via upload

---
+++ project/project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  project
+//
+//  Created by Olesia on 11/14/18.
+//  Copyright © 2018 Olesia. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
