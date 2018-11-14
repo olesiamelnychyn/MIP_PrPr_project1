5227bc2b369f5b50e3d6cdfa89e4eba387ec6035
Olesia <melnuchun@icloud.com>
2018-11-14 15:27:13 +0100

    Creating massive of structures

--- project/project/main.c
+++ project/project/main.c
@@ -3,6 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+//struktura:
+typedef struct
+{
+    char meno_priezvisko[50];
+    char pohlavie[50];
+    char rok_narodenia[50];
+    char SPZ[50];
+    char typ_priestupku[50];
+    char vyska_pokuty[50];
+    char datum_priestupku[50];
+} POLE_STRUKTUR;
+
 int main() {
 
     return 0;
