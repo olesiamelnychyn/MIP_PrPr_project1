7f42802a3b5971f8df5a216d3a0d30b8581c9056
Olesia <melnuchun@icloud.com>
2018-11-14 15:35:56 +0100

    Basic forms the function

--- project/project/main.c
+++ project/project/main.c
@@ -15,10 +15,70 @@ typedef struct
     char datum_priestupku[50];
 } POLE_STRUKTUR;
 
+//funkcie:
+void o_function(void);
+void a_function(void);
+void v_function(void);
+void r_function(void);
+void p_function(void);
+void x_function(void);
+void m_function(void);
+void s_function(void);
+void k_function(void);
+
 int main() {
     FILE *file;                    //file
     POLE_STRUKTUR *MASSIVE = NULL; //pole structur
     int K;                         //pocet zaznzmov
     char c;                        //znak pre funkcie
+    while ((c=getchar()))
+    {
+        switch (c)
+        {
+            case 'o':
+            {
+                o_function();
+                break;
+            }
+            case 'v':
+            {
+                v_function();
+                break;
+            }
+            case 'x':
+            {
+                x_function();
+                break;
+            }
+            case 'r':
+            {
+                r_function();
+                break;
+            }
+            case 'p':
+            {
+                p_function();
+                break;
+            }
+            case 'a':
+            {
+                a_function();
+                break;
+            }
+            case 'm':
+            {
+                m_function();
+                break;
+            }
+            case 's':
+                s_function();
+                break;
+            case 'k':
+            {
+                k_function();
+                break;
+            }
+        }
+    }
     return 0;
 }
